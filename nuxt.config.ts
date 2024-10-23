66688b90f0ff5c40574e8837107c1a254f60346d
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-23 14:04:09 +0200

    fix nuxt ui and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxt/ui"],
+	modules: ["@nuxt/eslint", "@nuxt/ui"],
 	devtools: { enabled: true },
 	compatibilityDate: "2024-04-03",
 	eslint: {
