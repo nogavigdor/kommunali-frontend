4716b98deafce114c403016b68d6626273d2abd5
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:33:36 +0100

    added map container component

---
+++ components/MapContainer.vue
@@ -0,0 +1,14 @@
+<!-- MapContainer.vue -->
+<template>
+  <div id="map" class="flex-grow h-full w-full">
+    <!-- Mapbox will mount here -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+#map {
+  height: calc(100vh - 104px); /* Adjust for navbars */
+}
+</style>
