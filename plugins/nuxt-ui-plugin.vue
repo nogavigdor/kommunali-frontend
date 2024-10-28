17d9a06353a747c828921b2de986b44fdaabbfad
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:50:29 +0100

    fixed extension name

--- plugins/nuxt-ui-plugin.vue
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from '#app'; import { UAppBar, UIcon, UTooltip,
-USidebar, UList, UListItem } from '@nuxt/ui'; export default
-defineNuxtPlugin((nuxtApp) => { nuxtApp.vueApp.component('UAppBar', UAppBar);
-nuxtApp.vueApp.component('UIcon', UIcon); nuxtApp.vueApp.component('UTooltip',
-UTooltip); nuxtApp.vueApp.component('USidebar', USidebar);
-nuxtApp.vueApp.component('UList', UList); nuxtApp.vueApp.component('UListItem',
-UListItem); });
