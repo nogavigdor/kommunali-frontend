0ab4a94c4b6985d68a905bd881e68f941f069a19
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:32:56 +0100

    added side bar component

---
+++ components/SidebarMenu.vue
@@ -0,0 +1,25 @@
+<!-- SidebarMenu.vue -->
+<template>
+  <USidebar :open="open" class="bg-secondary" @close="$emit('close')">
+    <UList>
+      <UListItem @click="$emit('go-to', 'setup')">
+        <UIcon name="settings" class="text-white" /> Setup
+      </UListItem>
+      <UListItem @click="$emit('go-to', 'profile')">
+        <UIcon name="person" class="text-white" /> Profile
+      </UListItem>
+      <UListItem @click="$emit('go-to', 'help')">
+        <UIcon name="help_circle" class="text-white" /> Help
+      </UListItem>
+    </UList>
+  </USidebar>
+</template>
+
+<script setup>
+import { USidebar, UList, UListItem, UIcon } from "@nuxt/ui";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  open: Boolean,
+});
+</script>
