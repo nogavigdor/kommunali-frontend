3b4302f4efd471ade0c2931ce15e7093aaac2f7c
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:34:02 +0100

    added homepage structure with child components

--- app.vue
+++ app.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex flex-col min-h-screen bg-primary">
+    <!-- Top Navbar -->
+    <TopNavbar @toggle-menu="toggleMenu" />
+
+    <!-- Sidebar Menu -->
+    <SidebarMenu :open="menuOpen" @close="menuOpen = false" @go-to="goTo" />
+
+    <!-- Map Container -->
+    <MapContainer />
+
+    <!-- Bottom Navigation -->
+    <BottomNavigation @go-to="goTo" />
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+const menuOpen = ref(false);
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value;
+};
+
+const router = useRouter();
+const goTo = (path) => {
+  router.push(path);
+};
+</script>
