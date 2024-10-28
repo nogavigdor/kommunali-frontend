af17e054f17236be08fce636353df00029c3f78c
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 16:02:18 +0100

    updated styling

--- pages/account.vue
+++ pages/account.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">Account Page</h1>
-    <p>Manage account details here.</p>
-  </div>
+	<div class="p-6">
+		<h1 class="text-2xl font-bold">
+			Account Page
+		</h1>
+		<p>Manage account details here.</p>
+	</div>
 </template>
 
 <script setup></script>
