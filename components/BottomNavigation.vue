19b98fb02e22cb987ddba73103c4b93d7e82526f
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:59:11 +0100

    Removed unnecessary Nuxt UI imports

--- components/BottomNavigation.vue
+++ components/BottomNavigation.vue
@@ -1,39 +1,42 @@
 <!-- BottomNavigation.vue -->
 <template>
-  <UAppBar
-    class="bottom-0 fixed w-full flex justify-between bg-primary text-white"
-  >
-    <button
-      class="flex-1 flex justify-center items-center"
-      @click="$emit('go-to', 'messages')"
-    >
-      <UIcon name="mail" class="text-xl text-white" />
-      <UTooltip>Account</UTooltip>
-    </button>
-    <button
-      class="flex-1 flex justify-center items-center"
-      @click="$emit('go-to', 'myStore')"
-    >
-      <UIcon name="storefront" class="text-xl text-white" />
-      <UTooltip>My Store</UTooltip>
-    </button>
-    <button
-      class="flex-1 flex justify-center items-center"
-      @click="$emit('go-to', 'wishlist')"
-    >
-      <UIcon name="heart" class="text-xl text-white" />
-      <UTooltip>Wishlist</UTooltip>
-    </button>
-    <button
-      class="flex-1 flex justify-center items-center"
-      @click="$emit('go-to', 'requests')"
-    >
-      <UIcon name="mail" class="text-xl text-white" />
-      <UTooltip>Requests</UTooltip>
-    </button>
-  </UAppBar>
+	<UAppBar
+		class="bottom-0 fixed w-full flex justify-between bg-primary text-white">
+		<button
+			class="flex-1 flex justify-center items-center"
+			@click="$emit('go-to', 'messages')">
+			<UIcon
+				name="mail"
+				class="text-xl text-white" />
+			<UTooltip>Account</UTooltip>
+		</button>
+		<button
+			class="flex-1 flex justify-center items-center"
+			@click="$emit('go-to', 'myStore')">
+			<UIcon
+				name="storefront"
+				class="text-xl text-white" />
+			<UTooltip>My Store</UTooltip>
+		</button>
+		<button
+			class="flex-1 flex justify-center items-center"
+			@click="$emit('go-to', 'wishlist')">
+			<UIcon
+				name="heart"
+				class="text-xl text-white" />
+			<UTooltip>Wishlist</UTooltip>
+		</button>
+		<button
+			class="flex-1 flex justify-center items-center"
+			@click="$emit('go-to', 'requests')">
+			<UIcon
+				name="mail"
+				class="text-xl text-white" />
+			<UTooltip>Requests</UTooltip>
+		</button>
+	</UAppBar>
 </template>
 
 <script setup>
-import { UAppBar, UIcon, UTooltip } from "@nuxt/ui";
+
 </script>
--- components/SidebarMenu.vue
+++ components/SidebarMenu.vue
@@ -1,25 +1,33 @@
 <!-- SidebarMenu.vue -->
 <template>
-  <USidebar :open="open" class="bg-secondary" @close="$emit('close')">
-    <UList>
-      <UListItem @click="$emit('go-to', 'setup')">
-        <UIcon name="settings" class="text-white" /> Setup
-      </UListItem>
-      <UListItem @click="$emit('go-to', 'profile')">
-        <UIcon name="person" class="text-white" /> Profile
-      </UListItem>
-      <UListItem @click="$emit('go-to', 'help')">
-        <UIcon name="help_circle" class="text-white" /> Help
-      </UListItem>
-    </UList>
-  </USidebar>
+	<USidebar
+		:open="open"
+		class="bg-secondary"
+		@close="$emit('close')">
+		<UList>
+			<UListItem @click="$emit('go-to', 'setup')">
+				<UIcon
+					name="settings"
+					class="text-white" /> Setup
+			</UListItem>
+			<UListItem @click="$emit('go-to', 'profile')">
+				<UIcon
+					name="person"
+					class="text-white" /> Profile
+			</UListItem>
+			<UListItem @click="$emit('go-to', 'help')">
+				<UIcon
+					name="help_circle"
+					class="text-white" /> Help
+			</UListItem>
+		</UList>
+	</USidebar>
 </template>
 
 <script setup>
-import { USidebar, UList, UListItem, UIcon } from "@nuxt/ui";
 import { defineProps } from "vue";
 
 const props = defineProps({
-  open: Boolean,
+	open: Boolean,
 });
 </script>
