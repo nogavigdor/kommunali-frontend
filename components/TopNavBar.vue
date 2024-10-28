a3fab021d761d3a7374f24a34d41871cd56ab357
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:33:21 +0100

    added top nav bar component

---
+++ components/TopNavBar.vue
@@ -0,0 +1,22 @@
+<!-- TopNavbar.vue -->
+<template>
+  <UAppBar class="bg-primary text-white">
+    <div class="flex justify-between items-center w-full">
+      <!-- Left Side - Menu Icon -->
+      <button class="ml-4" @click="$emit('toggle-menu')">
+        <UIcon name="menu" class="text-xl text-white" />
+      </button>
+      <!-- Right Side - Search Icon -->
+      <button class="mr-4">
+        <UIcon name="search" class="text-xl text-white" />
+        <UTooltip>Search</UTooltip>
+      </button>
+    </div>
+  </UAppBar>
+</template>
+
+<script setup>
+import { UAppBar, UIcon, UTooltip } from "@nuxt/ui";
+
+const _emit = defineEmits(["toggle-menu"]);
+</script>
