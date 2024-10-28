12c9ae7537467a29ae61fc64b0928e8b540aafa4
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-28 13:34:18 +0100

    defined color scheme in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
-	theme: {
-		extend: {
-			colors: {
-				primary: "#FF0000",
-				secondary: "#00FF00",
-				tertiary: "#0000FF",
-			},
-		},
-	},
-	plugins: [],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          light: "#c9f9c8", // Light green
+          DEFAULT: "#22c55e", // Default green
+          dark: "#15803d", // Dark green
+        },
+        secondary: {
+          light: "#fff3bf", // Soft yellow
+          DEFAULT: "#fbbf24", // Vibrant amber
+          dark: "#ea580c", // Warm orange
+        },
+      },
+      accent: {
+        light: "#fbcfe8", // Light coral
+        DEFAULT: "#e879f9", // Lavender
+        dark: "#c026d3", // Deep purple
+      },
+    },
+  },
+  plugins: [],
 };
