e7440b05b786cfdab10a95ec0c3a6a0f67bcfab5
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-11 13:25:44 +0100

    created UserLocation component with servers as an unput of the user location and retrieval of shops in bounderies according to the entered location

---
+++ components/UserLocation.vue
@@ -0,0 +1,38 @@
+<template>
+	<form>
+		<input
+			id="address-input"
+			type="text"
+			name="address"
+			autocomplete="shipping street-address">
+	</form>
+</template>
+
+<script setup lang="ts">
+import { MapboxAddressAutofill } from "@mapbox/search-js-web";
+
+const config = useRuntimeConfig();
+
+
+onMounted(() => {
+// instantiate a <mapbox-address-autofill> element using the MapboxAddressAutofill class
+	const autofillElement = new MapboxAddressAutofill();
+
+	autofillElement.accessToken = config.public.mapbox.accessToken;
+
+	// set the <mapbox-address-autofill> element's options
+	autofillElement.options = {
+		country: "dk",
+	};
+
+	const the_input = document.getElementById("address-input");
+	console.log(the_input);
+	const the_form = the_input?.parentElement;
+	console.log(the_form);
+
+	// append the <input> to <mapbox-address-autofill>
+	autofillElement.appendChild(the_input);
+	// append <mapbox-address-autofill> to the <form>
+	the_form.appendChild(autofillElement);
+});
+</script>
