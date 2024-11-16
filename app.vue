8fbb709101efdebfd87a499a71ce189a1328de1e
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-16 20:57:12 +0100

    added address change handler to UserLocation component and conditionally rendered MapContainer

--- app.vue
+++ app.vue
@@ -19,10 +19,10 @@
 
 		<NuxtPage />
 		<client-only>
-			<UserLocation />
+			<UserLocation @change-address="changeAddressHandler" />
 		</client-only>
 		<!-- Map Container -->
-		<MapContainer />
+		<MapContainer v-if="showMap" />
 
 		<!-- Bottom Navigation -->
 		<BottomNavigation @go-to="goTo" />
@@ -32,14 +32,25 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { useShopsStore } from "@/stores/shops";
 
 const menuOpen = ref(false);
 const toggleMenu = () => {
 	menuOpen.value = !menuOpen.value;
 };
 
+const showMap = ref(false);
+
 const router = useRouter();
 const goTo = (path) => {
 	router.push(path);
+};
+
+const shopsStore = useShopsStore();
+const changeAddressHandler = (mapboxAddressObject) => {
+	console.log("The selected address is:", mapboxAddressObject);
+	shopsStore.userLocation = mapboxAddressObject.features[0].geometry.coordinates;
+	console.log("The user location is:", shopsStore.userLocation);
+	showMap.value = true; // Trigger the map to be displayed
 };
 </script>
