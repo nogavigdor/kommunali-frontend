14d8a9d48bcf4214fd3cefec8314daf9b9aa67fe
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-16 02:03:56 +0100

    addederror handling in getShops function and add shopCount computed property

--- stores/shops.ts
+++ stores/shops.ts
@@ -2,30 +2,42 @@ import { ref } from "vue";
 import { defineStore } from "pinia";
 import type { IShop } from "@/types/shop";
 
-const config = useRuntimeConfig();
-
 export const useShopsStore = defineStore("shops", () => {
+	const config = useRuntimeConfig();
 	const shops = ref<IShop[]>([]);
-
 	const userLocation = ref<[number, number]>([0, 0]);
 
+	// Fetch shops within bounds based on user location
 	async function getShops() {
-		shops.value = await $fetch(`${config.public.apiBaseUrl}/api/stores/stores-in-bounds`,
-			{
-				headers: {
-					"Content-Type": "application/json",
+		try {
+			const response = await $fetch(`${config.public.apiBaseUrl}/api/stores/stores-in-bounds`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+					method: "POST",
+					body: {
+						bottomLeft: [userLocation.value[0] - 0.01, userLocation.value[1] - 0.01],
+						topRight: [userLocation.value[0] + 0.01, userLocation.value[1] + 0.01],
+					},
 				},
-				method: "POST",
-				body: {
-					bottomLeft: [userLocation.value[0] - 0.01, userLocation.value[1] - 0.01],
-					topRight: [userLocation.value[0] + 0.01, userLocation.value[1] + 0.01],
-				},
-			},
-		);
+			);
+
+			// Assuming response is an array of shops
+			shops.value = response as IShop[];
+		}
+		catch (error) {
+			console.error("Error fetching shops:", error);
+		}
 	}
+
+	// Computed property to get the number of shops fetched
+	const shopCount = computed(() => shops.value.length);
 
 	return {
 		shops,
+		userLocation,
 		getShops,
+		shopCount,
 	};
 });
