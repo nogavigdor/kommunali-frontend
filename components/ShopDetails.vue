e3a87126e13d5977d8570aeb519c37b794859e89
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-20 00:25:13 +0100

    Added ShopDetails component to display shop information and products

---
+++ components/ShopDetails.vue
@@ -0,0 +1,58 @@
+<!-- ShopDetails.vue -->
+<template>
+	<div class="absolute bottom-0 left-0 w-full bg-white p-4 shadow-medium rounded-t-2xl">
+		<div class="flex items-center justify-between">
+			<h2 class="text-2xl font-heading text-brandPrimary-700">
+				{{ shop.name }}
+			</h2>
+			<button
+				class="text-red-500 hover:text-red-700"
+				@click="$emit('close')">
+				Close
+			</button>
+		</div>
+		<p class="text-brandNeutral-dark">
+			{{ shop.description }}
+		</p>
+		<div class="overflow-x-auto flex gap-4 mt-4">
+			<div
+				v-for="product in shop.products"
+				:key="product._id"
+				class="absolute top-20 left-20 min-w-[200px] border border-brandNeutral-dark rounded-lg p-2 shadow-soft">
+				<img
+					:src="product.imageUrl || imagePlaceholder"
+					alt="Product Image"
+					class="w-full h-32 object-cover rounded-lg">
+				<h3 class="text-lg font-heading mt-2">
+					{{ product.name }}
+				</h3>
+				<p class="text-brandSecondary-dark">
+					{{ product.price }} DKK
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+import imagePlaceholder from "@/assets/images/image-placeholder.webp";
+
+const _props = defineProps({
+	shop: {
+		type: Object,
+		required: true,
+	},
+	style: {
+		type: Object,
+		required: true,
+	},
+});
+
+const _emit = defineEmits(["close"]);
+const placeholderImage = imagePlaceholder;
+</script>
+
+  <style scoped>
+
+  </style>
