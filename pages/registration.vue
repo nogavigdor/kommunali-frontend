888042bea6751a1eaf989449d395b4dc1ca57fa4
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-22 13:34:23 +0100

    added registration page with RegistrationForm component

---
+++ pages/registration.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="min-h-screen flex items-center justify-center bg-gradient-to-br from-brandPrimary-500 to-brandPrimary-800 text-white py-12 px-6 lg:px-8">
+		<RegistrationForm />
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+  <style scoped>
+
+  </style>
