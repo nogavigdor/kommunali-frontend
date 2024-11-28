e33117393f6bd86ec968f89db38772611628f377
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-28 17:23:14 +0100

    Added dynamic 'Add Shop' link to SidebarMenu if user has no shop

--- components/SidebarMenu.vue
+++ components/SidebarMenu.vue
@@ -2,25 +2,51 @@
 <template>
 	<UVerticalNavigation
 		:links="links"
-		class="absolute z-10 bg-secondary min-h-screen w-72 p-12 " />
+		class="absolute z-10 bg-secondary min-h-screen w-72 p-12 ">
+		<template #default="{ link }">
+			<span class="group-hover:text-primary relative">{{ link.label }}</span>
+		</template>
+	</UVerticalNavigation>
 </template>
 
 <script setup lang="ts">
-defineEmits(["close"]);
-const links = [{
-	label: "Profile",
-	avatar: {
-		src: "https://avatars.githubusercontent.com/u/739984?v=4",
-	},
-	badge: 100,
-}, {
-	label: "settings",
-	icon: "i-heroicons-settings",
-	to: "/settings",
-}, {
-	label: "help",
-	icon: "i-heroicons-help-circle",
-	to: "/help",
+import { computed } from "vue";
+import { useUserStore } from "@/stores/user";
 
-}];
+const userStore = useUserStore();
+const userHasShop = computed(() => userStore.user.stores.length > 0);
+
+// define a close emit event to close the sidebar
+defineEmits(["close"]);
+
+// Sidebar menu links
+const links = computed(() => {
+	const baseLinks = [{
+		label: "Profile",
+		avatar: {
+			src: "https://avatars.githubusercontent.com/u/739984?v=4",
+		},
+		badge: 100,
+	}, {
+		label: "settings",
+		icon: "i-heroicons-settings",
+		to: "/settings",
+	}, {
+		label: "help",
+		icon: "i-heroicons-help-circle",
+		to: "/help",
+
+	}];
+
+	// Add shop link if user has no shop
+	if (!userHasShop.value) {
+		baseLinks.push({
+			label: "Add Shop",
+			icon: "i-heroicons-plus-circle",
+			to: "/shop/new",
+		});
+	}
+
+	return baseLinks;
+});
 </script>
