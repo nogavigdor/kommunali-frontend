628037169d1c3bdb100a79a99680694ee4619a1b
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-29 04:11:35 +0100

    Added getUserShop, addShop, editShop and deleteShop functions

--- stores/shops.ts
+++ stores/shops.ts
@@ -1,11 +1,15 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import { useFirebaseAuth } from "vuefire";
 import type { IShop } from "@/types/shop";
+import type { IProduct } from "@/types/product";
 import type { ILocationQuery } from "~/types/locationQuery";
 
 export const useShopsStore = defineStore("shops", () => {
 	const config = useRuntimeConfig();
 	const shops = ref<IShop[]>([]);
+	const userShop = ref<IShop | null>(null);
+	const auth = useFirebaseAuth();
 
 	// Fetch shops within bounds based on user location
 	async function getShops(bounds: ILocationQuery) {
@@ -29,12 +33,104 @@ export const useShopsStore = defineStore("shops", () => {
 		}
 	}
 
+	async function getUserShop(shopId: string) {
+		try {
+			if (!auth) throw new Error("Firebase Auth not initialized");
+			const user = auth.currentUser;
+			if (!user) throw new Error("User not authenticated");
+
+			const token = await user.getIdToken();
+			const response = await $fetch(`/api/stores/${shopId}`, {
+				headers: {
+					"method": "GET",
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+
+			},
+			);
+			userShop.value = response as IShop;
+		}
+		catch (error) {
+			console.error("Failed to fetch user shop:", error);
+		}
+	}
+
+	async function addProduct(newProduct: IProduct) {
+		try {
+			if (!auth) throw new Error("Firebase Auth not initialized");
+			const user = auth.currentUser;
+			if (!user) throw new Error("User not authenticated");
+
+			const token = await user.getIdToken();
+			const response = await $fetch(`/api/stores/${userShop.value?._id}/products`, {
+				method: "POST",
+				headers: {
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+				body: newProduct,
+			});
+			userShop.value?.products.push(response as IProduct);
+		}
+		catch (error) {
+			console.error("Failed to add product:", error);
+		}
+	};
+
+	async function updateShop(updatedShop: IShop) {
+		try {
+			const user = auth?.currentUser;
+			if (!user) throw new Error("User not authenticated");
+
+			const token = await user.getIdToken();
+			const response = await $fetch(`/api/stores/${updatedShop._id}`, {
+				method: "PUT",
+				headers: {
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+				body: updatedShop,
+			});
+			const index = shops.value.findIndex(shop => shop._id === response._id);
+			shops.value[index] = response as IShop;
+		}
+		catch (error) {
+			console.error("Failed to update shop:", error);
+		}
+	}
+
+	async function deleteProduct(productId: string) {
+		try {
+			const user = auth?.currentUser;
+			if (!user) throw new Error("User not authenticated");
+
+			const token = await user.getIdToken();
+			await $fetch(`/api/stores/${userShop.value?._id}/products/${productId}`, {
+				method: "DELETE",
+				headers: {
+					"Authorization": `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			});
+			userShop.value!.products = userShop.value!.products.filter(product => product._id !== productId);
+		}
+		catch (error) {
+			console.error("Failed to delete product:", error);
+		}
+	}
+
 	// Computed property to get the number of shops fetched
 	const shopCount = computed(() => shops.value.length);
 
 	return {
 		shops,
 		getShops,
+		getUserShop,
+		addProduct,
+		updateShop,
+		deleteProduct,
+		userShop,
 		shopCount,
 	};
 });
