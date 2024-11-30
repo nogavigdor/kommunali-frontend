58b34af5dd3aeba3f28372a640939d56e3fcfdd3
Noga Vigdor <noga.vigdor@gmail.com>
2024-11-30 23:58:25 +0100

    added TypeScript support to the script setup

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">Profile Page</h1>
-    <p>View and edit profile information.</p>
-  </div>
+	<div class="p-6">
+		<h1 class="text-2xl font-bold">
+			Profile Page
+		</h1>
+		<p>View and edit profile information.</p>
+	</div>
 </template>
 
-<script setup></script>
+<script setup  lang="ts"></script>
