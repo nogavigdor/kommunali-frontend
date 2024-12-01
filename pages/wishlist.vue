dbf234738e93d20e9ed227364a451a307f0187bd
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-01 06:02:43 +0100

    added typescript support

--- pages/wishlist.vue
+++ pages/wishlist.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">Wishlist Page</h1>
-    <p>View and manage items one wish to buy.</p>
-  </div>
+	<div class="p-6">
+		<h1 class="text-2xl font-bold">
+			Wishlist Page
+		</h1>
+		<p>View and manage items one wish to buy.</p>
+	</div>
 </template>
 
-<script setup></script>
+<script setup lang="ts"></script>
