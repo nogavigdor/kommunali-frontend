bba952c79dab4ebc089da6b8723c03593edcf885
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-01 06:02:54 +0100

    added typescript support

--- pages/setting.vue
+++ pages/setting.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="p-6">
-    <h1 class="text-2xl font-bold">Setup Page</h1>
-    <p>Here one can configure the application settings.</p>
-  </div>
+	<div class="p-6">
+		<h1 class="text-2xl font-bold">
+			Setup Page
+		</h1>
+		<p>Here one can configure the application settings.</p>
+	</div>
 </template>
+
+<script setup lang="ts"></script>
