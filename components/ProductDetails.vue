805e23d336bd1daea1c362a01d8cb9a001edbc62
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-05 23:50:28 +0100

    created basic structure for product details component

---
+++ components/ProductDetails.vue
@@ -0,0 +1,20 @@
+<template>
+	<div />
+</template>
+
+<script setup lang="ts">
+import { useShopsStore } from "@/stores/shops";
+import type { IProduct } from "@/types/product";
+
+const props = defineProps<{
+	productId: string;
+}>();
+
+const shopsStore = useShopsStore();
+const product = ref<IProduct | null>(null);
+
+product.value = shopsStore.getUserShopProduct(props.productId) || null;
+</script>
+
+<style scoped>
+</style>
