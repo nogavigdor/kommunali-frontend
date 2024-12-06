0ae21f47137b201bf6b5ecd852141d8149bb467a
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-06 16:34:32 +0100

    added dynamic input handling for various field types and image upload functionality

--- components/EditField.vue
+++ components/EditField.vue
@@ -1,8 +1,15 @@
 <template>
 	<div class="flex items-center">
 		<div v-if="!editing">
-			<span>{{ value }}</span>
+			<span v-if="fieldType !== 'image'">{{ fieldValue }}</span>
+			<img
+				v-if="fieldType === 'image'"
+				:src="fieldValue"
+				alt="Image"
+				class="w-20 h-20 object-cover rounded-md">
+
 			<button
+				v-if="loggedIn"
 				class="ml-2"
 				@click="edit">
 				<Icon
@@ -10,10 +17,40 @@
 					style="color: black" />
 			</button>
 		</div>
+		<!-- Editing state -->
 		<div v-else>
-			<input
-				v-model="editableValue"
-				class="border border-neutral-300 rounded-md shadow-sm focus:ring-brandPrimary-500 focus:border-brandPrimary-500 sm:text-sm">
+			<template v-if="fieldType === 'text' || fieldType === 'number'">
+				<input
+					v-model="editableValue"
+					:type="fieldType"
+					class="border border-neutral-300 rounded-md shadow-sm focus:ring-brandPrimary-500 focus:border-brandPrimary-500 sm:text-sm">
+			</template>
+
+			<template v-if="fieldType === 'select'">
+				<select
+					v-model="editableValue"
+					class="border border-neutral-300 rounded-md shadow-sm focus:ring-brandPrimary-500 focus:border-brandPrimary-500 sm:text-sm">
+					<option
+						v-for="option in options"
+						:key="option"
+						:value="option">
+						{{ option }}
+					</option>
+				</select>
+			</template>
+
+			<template v-if="fieldType === 'image'">
+				<input
+					type="file"
+					accept="image/*"
+					class="mb-2"
+					@change="handleImageUpload">
+				<button
+					class="ml-2 btn btn-primary"
+					@click="takePicture">
+					Take Picture
+				</button>
+			</template>
 			<button
 				class="ml-2"
 				@click="save">
@@ -26,19 +63,26 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from "firebase/storage";
 import { ref, watch, defineProps, defineEmits } from "vue";
+import { useUserStore } from "~/stores/user";
 
 const props = defineProps<{
-	value?: string;
-	field: string;
+	fieldType: "text" | "number" | "select" | "image";
+	fieldValue?: string;
+	fieldName: string;
+	options?: string[];
 }>();
 
-const emits = defineEmits(["updateField"]);
+const emits = defineEmits(["updateField", "updateImage", "takePicture", "handleImageUpload"]);
+
+const loggedIn = storeToRefs(useUserStore()).loggedIn;
 
 const editing = ref(false);
-const editableValue = ref(props.value);
+const editableValue = ref(props.fieldValue);
 
-watch(() => props.value, (newValue) => {
+watch(() => props.fieldValue, (newValue) => {
 	editableValue.value = newValue;
 });
 
@@ -48,7 +92,12 @@ function edit() {
 
 function save() {
 	editing.value = false;
-	emits("updateField", { field: props.field, value: editableValue.value });
+	emits("updateField", { fieldName: props.fieldName, fieldValue: editableValue.value });
+}
+
+function cancel() {
+	editing.value = false;
+	editableValue.value = props.fieldValue;
 }
 </script>
 
