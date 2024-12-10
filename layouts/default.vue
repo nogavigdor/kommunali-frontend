b27a65f07079250b636af9fc5d168f065a0222a0
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-10 23:52:56 +0100

    moved app content to a new default layout file

---
+++ layouts/default.vue
@@ -0,0 +1,153 @@
+<template>
+	<div class="flex flex-col min-h-screen bg-primary">
+		<!-- Top Navbar -->
+		<TopNavBar
+			:open="menuOpen"
+			@toggle-menu="toggleMenu" />
+		<!-- @highlight-shops="mapRef?.value?.highlightMarkers && mapRef.value.highlightMarkers()" / -->
+
+		<!-- Sidebar Menu -->
+		<transition
+			enter-active-class="transition-transform duration-500 ease-in-out"
+			leave-active-class="transition-transform duration-500 ease-in-out"
+			enter-from-class="-translate-x-full"
+			enter-to-class="translate-x-0"
+			leave-from-class="translate-x-0"
+			leave-to-class="-translate-x-full">
+			<SidebarMenu
+				v-if="menuOpen"
+				@close="menuOpen = false" />
+		</transition>
+		<!-- General Feedback Message Component -->
+		<FeedbackMessage />
+		<client-only>
+			<UserLocation
+				v-if="showUserLocation"
+				ref="userLocationRef"
+				@change-address="changeAddressHandler" />
+		</client-only>
+		<FeedbackMeesage />
+		<NuxtPage />
+
+		<!-- Map Container -->
+		<MapContainer
+			ref="map"
+			:is-hidden="!showMap" />
+
+		<!-- Bottom Navigation -->
+		<BottomNavigation @go-to="goTo" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { useFirebaseAuth } from "vuefire";
+import { storeToRefs } from "pinia";
+import type { IShop } from "./types/shop";
+import { useUserStore } from "@/stores/user";
+import { useShopsStore } from "@/stores/shops";
+import type { IUser } from "@/types/user";
+
+const menuOpen = ref(false);
+const toggleMenu = () => {
+	menuOpen.value = !menuOpen.value;
+};
+
+const shopsStore = useShopsStore();
+
+const auth = useFirebaseAuth()!;
+
+const showMap = ref(false);
+
+const showUserLocation = ref(true);
+
+const userLocationRef = ref(null); // Add reference to UserLocation component
+const mapRef = ref(null); // Add reference to Map component
+
+const isMobile = ref(false);
+
+const showPage = ref(true);
+
+// Detect if the view is on mobile
+const detectMobile = () => {
+	isMobile.value = window.innerWidth <= 768;
+};
+
+// Import the current route
+const route = useRoute(); // This line is crucial!
+
+const router = useRouter();
+const goTo = (path) => {
+	router.push(path);
+};
+
+const userStore = useUserStore();
+const { user, userLocation, loggedIn, firebaseUserId, hasShop } = storeToRefs(userStore);
+const changeAddressHandler = (mapboxAddressObject) => {
+	console.log("The selected address is:", mapboxAddressObject);
+	userStore.userLocation = mapboxAddressObject.features[0].geometry.coordinates;
+	console.log("The user location is:", userStore.userLocation);
+	showMap.value = true; // Trigger the map to be displayed
+	showPage.value = false; // Hide the homepage content
+};
+
+// Watch the route to determine if it's the homepage or another page
+watch(
+	() => route.path,
+	(newPath) => {
+		// If mobile and not on homepage, hide the map and user location
+		if (isMobile.value && newPath !== "/") {
+			showMap.value = false;
+			showUserLocation.value = false;
+			showPage.value = true;
+		}
+		else {
+			showMap.value = true; // Show the map if on homepage
+			showUserLocation.value = true; // Show the user location if on homepage
+			showPage.value = true; // Hide the homepage content if on another page
+		}
+	},
+);
+onMounted(() => {
+	// check if user still is logged in firbase authentication and update the user store accordingly
+	onAuthStateChanged(auth, async (firebaseUser) => {
+		if (firebaseUser) {
+			console.log("User detected on page load:", firebaseUser);
+			loggedIn.value = true;
+			firebaseUserId.value = firebaseUser.uid;
+			try {
+				const userResponse: IUser = await userStore.getUser(firebaseUserId.value);
+				user.value = userResponse;
+				userLocation.value = userResponse.lastCoordinates;
+				showMap.value = true;
+
+				if (userResponse.stores && userResponse.stores.length > 0) {
+					const shopObj = userResponse.stores[0];
+					const shopId = shopObj._id;
+					user.value.storesId = [shopId];
+					shopsStore.userShop = shopObj;
+
+					hasShop.value = true;
+				}
+			}
+			catch (error) {
+				console.error("Error fetching user after refresh:", error);
+			}
+		}
+		else {
+			loggedIn.value = false;
+		}
+	});
+	detectMobile();
+	window.addEventListener("resize", detectMobile);
+
+	// Function to scroll to the input in UserLocation
+	const scrollToUserLocationInput = () => {
+		if (userLocationRef.value) {
+			userLocationRef.value.scrollToInput(); // Call the function from UserLocation
+		}
+	};
+});
+</script>
