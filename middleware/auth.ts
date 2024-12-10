75c60d067bbd878f05535370fe7fd68a61ee5b81
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-10 23:55:18 +0100

    Added authentication middleware to protect pages that require loggin

---
+++ middleware/auth.ts
@@ -0,0 +1,15 @@
+import { useUserStore } from "@/stores/user";
+
+export default defineNuxtRouteMiddleware((to, from) => {
+	const userStore = useUserStore();
+	// If the user is not logged in and the route requires user authentication,
+	// redirect to the login page
+	if (!userStore.loggedIn && to.meta.requiresUserAuth) {
+		return navigateTo("/login");
+	}
+	// If the user is not an admin and the route requires admin authentication,
+	// redirect to the home page
+	if (!userStore.isAdmin && to.meta.requiresAdminAuth) {
+		return navigateTo("/");
+	}
+});
