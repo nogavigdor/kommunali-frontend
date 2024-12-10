7ae837d2c4546c5e64791508e2516a365c2c380e
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-10 23:57:42 +0100

    Add authentication layout with navigation to homepage

---
+++ layouts/auth.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="auth-layout">
+		<NuxtPage />
+		<button @click="goToHome">
+			Go to Homepage
+		</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const goToHome = () => {
+	useRouter().push("/");
+};
+</script>
