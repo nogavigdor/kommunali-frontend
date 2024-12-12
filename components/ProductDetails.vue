784fca8bc797dfaf327a91fb276f3116457985cc
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-12 15:47:20 +0100

    fixed reactivity issue in order to display updated product details - replaced props.product with a reactive currentProduct  reference to track changes and then implemented a watcher on userShop.products to update `currentProduct` dynamically when product details change. By that I've ensured real-time updates in the ProductDetails component after editing a product and improved reactivity by reflecting changes immediately without manual refresh.

--- components/ProductDetails.vue
+++ components/ProductDetails.vue
@@ -2,14 +2,14 @@
 	<div
 		class=" min-w-[150px] max-w-xs  border border-neutral-dark rounded-lg p-2 shadow-soft">
 		<img
-			:src="product.imageUrl ? product.imageUrl : imagePlaceholder"
-			:alt="product.name"
+			:src="currentProduct.imageUrl ? currentProduct.imageUrl : imagePlaceholder"
+			:alt="currentProduct.name"
 			class="w-full h-32 object-cover rounded-lg">
 		<h3 class="text-lg font-heading mt-2">
-			{{ product.name }}
+			{{ currentProduct.name }}
 		</h3>
 		<p class="text-brandSecondary-dark">
-			{{ product.price }} DKK
+			{{ currentProduct.price }} DKK
 		</p>
 		<button
 			v-if="editable"
@@ -18,7 +18,7 @@
 		</button>
 		<ProductDetailsEditModal
 			v-model="openModal"
-			:product="product" />
+			:product="currentProduct" />
 	</div>
 </template>
 
@@ -27,12 +27,37 @@ import { useShopsStore } from "@/stores/shops";
 import type { IProduct } from "@/types/product";
 import imagePlaceholder from "@/assets/images/image-placeholder.webp";
 
+const shopStore = useShopsStore();
+
 const props = defineProps<{
 	product: IProduct;
 	editable: boolean;
 }>();
 
 const openModal = ref(false);
+
+// Create a local reference for the product
+const currentProduct = ref({ ...props.product });
+
+// Watch the userShop state and update the current product when the product updates.
+// Whenever the userShop is updated with new product data, the currentProduct is synchronized
+watch(
+	() => shopStore.userShop?.products,
+	(updatedProducts) => {
+		if (updatedProducts) {
+			const updatedProduct = updatedProducts.find(
+				(p: IProduct) => p._id === props.product._id,
+			);
+			if (updatedProduct) {
+				currentProduct.value = { ...updatedProduct };
+			}
+		}
+	},
+	{ // Ensures the watcher runs when the component is mounted, setting the initial value of currentProduct
+		immediate: true,
+		// Ensures the watcher tracks changes to the entire products array
+		deep: true },
+);
 
 // const shopsStore = useShopsStore();
 </script>
