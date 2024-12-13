bec11e6d51b911c8ceef4274baa3ca5da1a70f9f
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-13 21:43:11 +0100

    Refactored auth middleware to use user retrieval from Firebase and remove dependency on userStore for authentication checks.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,20 +1,25 @@
 import { useUserStore } from "@/stores/user";
 
-export default defineNuxtRouteMiddleware((to, from) => {
-	const userStore = useUserStore();
+export default defineNuxtRouteMiddleware(async (to, from) => {
+	// Get the current user from firebase
+	const user = await getCurrentUser();
+	// uncommented since cant user the loggedIn value since userStore state are not
+	// yet updated with the loggedIn value
+	// const userStore = useUserStore();
+
 	// If the user is not logged in and the route requires user authentication,
 	// redirect to the login page
-	if (!userStore.loggedIn && to.meta.requiresUserAuth) {
+	if (!user && to.meta.requiresUserAuth) {
 		return navigateTo("/login");
 	}
 	// If the user is not an admin and the route requires admin authentication,
 	// redirect to the home page
-	if (!userStore.isAdmin && to.meta.requiresAdminAuth) {
+	if (!user && to.meta.requiresAdminAuth) {
 		return navigateTo("/");
 	}
 	// If the route is the loggin page, but the user is logged in,
 	// redirect to the home page
-	if (to.path === "/login" && userStore.loggedIn) {
+	if (to.path === "/login" && user) {
 		return navigateTo("/");
 	}
 });
