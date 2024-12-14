9158dd52a668f9ad5732ea69fd36958de86e6fbc
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-14 23:41:49 +0100

    Refactored ProductAddModal to integrate the ImageUpload component for image handling (gets the setImageUrl emit from ImageUpload and triggers the setImageUrl to update the newProductData that is will be sent as the new product object once the "Save Product" is clicked (I've updated the button text to 'Save Product' instaed of "Add Product").

--- components/ProductAddModal.vue
+++ components/ProductAddModal.vue
@@ -40,20 +40,24 @@
 				<label
 					for="edit-product-image-url"
 					class="block text-sm font-medium text-neutral-dark">Image URL</label>
-				<input
-					id="edit-product-image-url"
-					v-model="newProductData.imageUrl"
-					type="text"
-					class="mt-1 block w-full border border-neutral-300 rounded-md shadow-sm focus:ring-brandPrimary-500 focus:border-brandPrimary-500 sm:text-sm">
+				<ImageUpload @image-uploaded="setImageUrl" />
+				<p
+					v-if="newProductData.imageUrl"
+					class="mt-2 text-sm text-neutral-dark">
+					Image URL: <a
+						:href="newProductData.imageUrl"
+						target="_blank"
+						class="text-brandPrimary-500 hover:underline">{{ newProductData.imageUrl }}</a>
+				</p>
+				<button
+					type="submit"
+					class="w-full bg-brandPrimary-500 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium hover:bg-brandPrimary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brandPrimary-500">
+					<Iccon
+						name="uli:plus-circle"
+						class="text-xl" />
+					Save Product
+				</button>
 			</div>
-			<button
-				type="submit"
-				class="w-full bg-brandPrimary-500 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium hover:bg-brandPrimary-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brandPrimary-500">
-				<Iccon
-					name="uli:plus-circle"
-					class="text-xl" />
-				Add Product
-			</button>
 		</form>
 	</UModal>
 </template>
@@ -77,6 +81,11 @@ const newProductData = ref<IProduct>({
 	reservedExpiration: null,
 	soldTo: undefined,
 });
+
+// Set the uploaded image URL
+const setImageUrl = (url: string) => {
+	newProductData.value.imageUrl = url;
+};
 
 const addProduct = async () => {
 	try {
