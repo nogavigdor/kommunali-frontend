e9c145f9b111eaaed918ee28dc134926f01d281d
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-28 11:14:44 +0100

    Refactored authentication middleware to use updated user store properties for login and admin checks

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -8,17 +8,17 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
 	// If the user is not logged in and the route requires user authentication,
 	// redirect to the login page
-	if (!userStore.isLogged && to.meta.requiresUserAuth) {
+	if (!userStore.loggedIn && to.meta.requiresUserAuth) {
 		return navigateTo("/login");
 	}
 	// If the user is not an admin and the route requires admin authentication,
 	// redirect to the home page
-	if (userStore.user.role !== Admin && to.meta.requiresAdminAuth) {
+	if (userStore.user.role !== "admin" && to.meta.requiresAdminAuth) {
 		return navigateTo("/");
 	}
 	// If the route is the loggin page, but the user is logged in,
 	// redirect to the home page
-	if (to.path === "/login" && user.loggedId) {
+	if (to.path === "/login" && userStore.loggedIn) {
 		return navigateTo("/");
 	}
 });
