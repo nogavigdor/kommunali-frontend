daa7eeff4399cb74d39735e578232211c0d456c4
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-28 15:56:03 +0100

    Changed v-if to v-show for improved rendering performance in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		v-if="showPage"
+		v-show="showPage"
 		class="relative min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-brandPrimary-500 to-brandPrimary-800 text-white overflow-hidden">
 		<!-- Main Content -->
 		<div class="relative z-10 text-center px-6 max-w-2xl animate-slide-in-down">
