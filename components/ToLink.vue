03267f2d81b75beac897925d026706dac36e33bd
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-29 14:04:14 +0100

    Refactored ToLink component to use NuxtLink for navigation and simplified props handling for improved clarity and performance.

--- components/ToLink.vue
+++ components/ToLink.vue
@@ -1,32 +1,23 @@
 <template>
-	<UTooltip text="props.tooltipText">
-		<button
-
+	<UTooltip :text="tooltipText">
+		<NuxtLink
+			:to="link"
 			class=" hover:text-green-700"
-			aria-label="Back to Home Page"
-			@click="toLink">
+			aria-label="Back to Home Page">
 			<Icon
-				:name="'uil:' + props.icon"
+				:name="'uil:' + icon"
 				class="
 				text-4xl
 				font-bold
 				hover:text-green-700" />
-		</button>
+		</NuxtLink>
 	</UTooltip>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from "vue-router";
-
-const props = defineProps<{
+defineProps<{
 	link: string;
 	icon: string;
 	tooltipText?: string; // Optional prop for the tooltip text
 }>();
-
-const router = useRouter();
-
-const toLink = () => {
-	router.push(props.link);
-};
 </script>
