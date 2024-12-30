17dc9961b0038785c7dc12a3bfb188afe8ece05e
Noga Vigdor <noga.vigdor@gmail.com>
2024-12-30 10:32:19 +0100

    minor cosmetic change.

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
 // app.config.ts for Nuxt UI theme
 export default defineAppConfig({
 	ui: {
-		primary: "brandPrimary", // Referring to the custom brandPrimary defined in Tailwind config
-		gray: "brandGray", // Referring to the custom brandGray defined in Tailwind config
+		// Referring to the custom brandPrimary defined in Tailwind config
+		primary: "brandPrimary",
+		// Referring to the custom brandGray defined in Tailwind config
+		gray: "brandGray",
 	},
 });
