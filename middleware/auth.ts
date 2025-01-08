d84e0c26381b0e266796576e69238c9bb69178db
Noga Vigdor <noga.vigdor@gmail.com>
2025-01-08 12:43:50 +0100

    Updated authentication middleware to check user session and updated route middleware logic. Ennabled session cookies in Nuxt config.

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -2,23 +2,25 @@ import { useUserStore } from "@/stores/user";
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
 	const userStore = useUserStore();
+	const user = await getCurrentUser();
+
 	// uncommented since cant user the loggedIn value since userStore state are not
 	// yet updated with the loggedIn value
 	// const userStore = useUserStore();
 
 	// If the user is not logged in and the route requires user authentication,
 	// redirect to the login page
-	if (!userStore.loggedIn && to.meta.requiresUserAuth) {
+	if (!user && to.meta.requiresUserAuth) {
 		return navigateTo("/login");
 	}
 	// If the user is not an admin and the route requires admin authentication,
 	// redirect to the home page
-	if (userStore.user.role !== "admin" && to.meta.requiresAdminAuth) {
+	if (user && userStore.user.role !== "admin" && to.meta.requiresAdminAuth) {
 		return navigateTo("/");
 	}
 	// If the route is the loggin page, but the user is logged in,
 	// redirect to the home page
-	if (to.path === "/login" && userStore.loggedIn) {
+	if (to.path === "/login" && user) {
 		return navigateTo("/");
 	}
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -97,6 +97,7 @@ export default defineNuxtConfig({
 		},
 		auth: {
 			enabled: true,
+			sessionCookie: true,
 		},
 	},
 });
