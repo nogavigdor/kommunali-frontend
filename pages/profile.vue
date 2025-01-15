3e8ad2277a8205cfd684578666d8cb2dadea030f
Noga Vigdor <noga.vigdor@gmail.com>
2025-01-15 19:16:38 +0100

    Added nickname editing functionality to profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -10,10 +10,41 @@
 				tooltip-text="Back to Home Page" />
 		</div>
 		<p>View and edit profile information.</p>
+		<label
+			for="nickname"
+			class="block mt-4">Your Nickname:</label>
+		<EditField
+			id="nickname"
+			:field-type="'text'"
+			:field-name="'nickname'"
+			:field-value="user.nickname"
+			@update-field="updateUserField" />
 	</div>
 </template>
 
 <script setup  lang="ts">
+import { useUserStore } from "@/stores/user";
+import type { IUser } from "@/types/user";
+
+const userStore = useUserStore();
+
+const user = computed(() => userStore.user);
+
+async function updateUserField({ fieldName, fieldValue }: { fieldName: keyof IUser; fieldValue: string }) {
+	if (user.value) {
+		const updatedData = { [fieldName]: fieldValue, _id: user.value._id } as Partial<IUser> & { _id: string };
+
+		try {
+			console.log("Updating user...", updatedData);
+			await userStore.updateUser(updatedData);
+			console.log("User updated successfully");
+		}
+		catch (error) {
+			console.error("Error updating user:", error);
+		}
+	}
+}
+
 definePageMeta({
 	middleware: "auth",
 	title: "Profile",
